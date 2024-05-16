9578b81231e40c5f15d69ec7302d98d424501005
Meet Rich <fducom@gmail.com>
2024-05-16 08:42:47 +0300

    fix cart not full and add pages to admin panel

---
+++ .history/pages/content/[slug]_20240516014416.jsx
@@ -0,0 +1,34 @@
+
+
+// export default function PrivacyPage () {
+
+//     return (
+//         <div className="container">This is privacy page!</div>
+//     )
+// }
+
+
+import getData from '@/queries/getData';
+import { SinglePageQuery } from '@/queries/SinglePageQueries';
+
+export default function PrivacyPage({ title, content }) {
+  return (
+    <>
+      <div className="container">
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: content }} />
+      </div>
+    </>
+  );
+}
+export const getServerSideProps = async (ctx) => {
+    const {slug} = ctx.query;
+    // Call your function to search for the page in the database using the slug
+    const data = await getData(SinglePageQuery, 'pages', { page_slug: slug });
+    return {
+        props: {
+            title: data[0].title??"Title",
+            content: data[0].content??"Create page with slug:" + slug,
+        },
+    };
+};
---
+++ .history/pages/privacy/index_20240515231200.jsx
@@ -0,0 +1,8 @@
+
+
+export default function PrivacyPage () {
+
+    return (
+        <div className="container">This is privacy page!</div>
+    )
+}
